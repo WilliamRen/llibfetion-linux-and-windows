d03012835a092f1c8f3cca2669c2a7e4c863532f
programmeboy <programmeboy@23033794-a597-11de-b32a-69fa65287987>
2010-10-02 05:50:39 +0000

    del for new version

--- libfetion/xml.h
+++
@@ -1,9 +0,0 @@
-#ifndef XML_H_INCLUDED
-#define XML_H_INCLUDED
-
-FX_RET_CODE fx_parse_user_conf( __in  struct mem_struct* mem, \
-                                __out struct login_data* l_data );
-FX_RET_CODE fx_parse_sys_conf( __in  struct mem_struct* mem, \
-                               __out struct sys_conf_data* sys_data );
-
-#endif // XML_H_INCLUDED
